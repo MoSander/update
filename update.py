bcc73c228672447d5ac45e6e672d62d72db74345
Felix Kruger <momo.sander@googlemail.com>
2012-07-23 17:47:17 +0100

    commiting before merging with fakrepo

--- update.py
+++ update.py
@@ -14,17 +14,17 @@ def updateChEMBL(release, user, pword, host, port):
   import os
   import sys
    
-  os.system("wget ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_%s/chembl_%s_mysql.tar.gz" %(release, release))
+  #os.system("ftp ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_%s/chembl_%s_mysql.tar.gz" %(release, release))
   os.system("tar -zxvf chembl_%s_mysql.tar.gz" % release)
-  os.system("mysqladmin -u%s -p%s -h%s -P%s create chembl_%s" %(user, pword, host, port, release))    
-  os.system("mysql -u%s -p%s -h%s -P%s chembl_%s < chembl_%s_mysql/chembl_%s.mysqldump.sql" % ( user, pword, host, port, release, release, release))
+  os.system("mysqladmin5 -u%s -p%s -h%s -P%s create chembl_%s" %(user, pword, host, port, release))    
+  os.system("mysql5 -u%s -p%s -h%s -P%s chembl_%s < chembl_%s_mysql/chembl_%s.mysqldump.sql" % ( user, pword, host, port, release, release, release))
 
                                   	                                                     
 if __name__ == '__main__':
   import sys                                        
   import os
   if len(sys.argv) != 6:                  
-    print "specify path, release, user, pword, host, port"
+    print "specify  release, user, pword, host, port"
     sys.exit()                                                                             
                                         
   release = str(sys.argv[1])
